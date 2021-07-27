bd4502d11ff36aaadb6934fc9a4e18ccfe600cc7
Fabrice Madre <stepwiseltd@gmail.com>
2021-07-27 14:46:25 +0400

    basics of CRUD implemented

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,50 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+    flash[:notice] = 'Task has been successfully created'
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_completed_params)
+    @task.save
+    flash[:notice] = 'Task has been updated successfully'
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+    @task.save
+    flash[:notice] = 'Task has been successfully deleted'
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details)
+  end
+
+  def task_completed_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get    "tasks",          to: "tasks#index",  as: :tasks
-  # post   "restaurants",          to: "restaurants#create"
-  # get    "restaurants/new",      to: "restaurants#new",    as: :new_restaurant
-  # get    "restaurants/:id/edit", to: "restaurants#edit",   as: :edit_restaurant
-  # get    "restaurants/:id",      to: "restaurants#show",   as: :restaurant
-  # patch  "restaurants/:id",      to: "restaurants#update"
-  # delete "restaurants/:id",      to: "restaurants#destroy"
+  get 'tasks', to: 'tasks#index', as: :tasks
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
